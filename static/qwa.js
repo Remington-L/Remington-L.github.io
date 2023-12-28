8f81bbb2e46416cb7f4b25d2630419ba4e849c90
Remington <remington7231@gmail.com>
2023-12-27 22:09:05 -0600

    aaa

--- static/qwa.js
+++ static/qwa.js
@@ -1,50 +1,54 @@
 const dragArea = document.querySelector('.drag-area');
 const dragText = document.querySelector('.header');
 
+let file;
+
 // when file is inside drag area
 
 dragArea.addEventListener('dragenter', (ev) => {
     ev.preventDefault();
     dragText.textContent = 'Release to Upload';
     console.log('File is inside the drag area');
+    dragArea.classList.add('active');
 });
 
 dragArea.addEventListener('dragleave', () => {
     console.log('File left the drag area');
     dragText.textContent = 'Drag & Drop';
+    dragArea.classList.remove('active');
 });
 
 dragArea.addEventListener('drop', (ev) => {
     ev.preventDefault();
     console.log('The file is dropped in drag area');
+    file = ev.dataTransfer.files[0];
+    console.log(file);
 });
 
-function dragOverHandler(ev) {
-    console.log("File(s) in drop zone");
-  
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-  }
 
-  function dropHandler(ev) {
-    console.log("File(s) dropped");
-  
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-  
-    if (ev.dataTransfer.items) {
-      // Use DataTransferItemList interface to access the file(s)
-      [...ev.dataTransfer.items].forEach((item, i) => {
-        // If dropped items aren't files, reject them
-        if (item.kind === "file") {
-          const file = item.getAsFile();
-          console.log(`… file[${i}].name = ${file.name}`);
-        }
-      });
-    } else {
-      // Use DataTransfer interface to access the file(s)
-      [...ev.dataTransfer.files].forEach((file, i) => {
-        console.log(`… file[${i}].name = ${file.name}`);
-      });
-    }
+var dropzoneId = "dropzone";
+
+window.addEventListener("dragenter", function(e) {
+  if (e.target.id != dropzoneId) {
+    e.preventDefault();
+    e.dataTransfer.effectAllowed = "none";
+    e.dataTransfer.dropEffect = "none";
   }
+}, false);
+
+window.addEventListener("dragover", function(e) {
+  if (e.target.id != dropzoneId) {
+    e.preventDefault();
+    e.dataTransfer.effectAllowed = "none";
+    e.dataTransfer.dropEffect = "none";
+  }
+});
+
+window.addEventListener("drop", function(e) {
+  if (e.target.id != dropzoneId) {
+    e.preventDefault();
+    
+    
+    console.log('The file is dropped in drag area');
+  }
+});
