619dd3c92e5026805322108d5a1fdbb58a25a7a3
Remington-L <148765185+Remington-L@users.noreply.github.com>
2023-12-27 23:11:14 -0600

    Add files via upload

--- qwa.js
+++ qwa.js
@@ -26,29 +26,3 @@ dragArea.addEventListener('drop', (ev) => {
 });
 
 
-var dropzoneId = "dropzone";
-
-window.addEventListener("dragenter", function(e) {
-  if (e.target.id != dropzoneId) {
-    e.preventDefault();
-    e.dataTransfer.effectAllowed = "none";
-    e.dataTransfer.dropEffect = "none";
-  }
-}, false);
-
-window.addEventListener("dragover", function(e) {
-  if (e.target.id != dropzoneId) {
-    e.preventDefault();
-    e.dataTransfer.effectAllowed = "none";
-    e.dataTransfer.dropEffect = "none";
-  }
-});
-
-window.addEventListener("drop", function(e) {
-  if (e.target.id != dropzoneId) {
-    e.preventDefault();
-    
-    
-    console.log('The file is dropped in drag area');
-  }
-});
